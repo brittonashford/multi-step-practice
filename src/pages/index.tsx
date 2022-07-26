3ed0b2149dcd6392e9cfcc5a26ecb6fc3a0cec1e
Bret Kitchell <brittonashford@gmail.com>
2022-07-26 14:45:20 -0500

    added FomikStepper and Formikstep, moved validation schema

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
 import { Card, CardContent, Typography } from "@material-ui/core";
 import { getThemeProps } from "@material-ui/styles";
-import { Formik, Form, Field, FormikConfig, FormikValues } from "formik";
+import { Formik, Form, Field, FormikConfig, FormikValues, Button } from "formik";
 import { CheckboxWithLabel, TextField } from "formik-material-ui";
 import React, { useState } from "react";
+import { childrenToReact } from "react-markdown/lib/ast-to-react";
 import { mixed, number, object } from "yup";
 
 export default function Home() {
@@ -10,15 +11,7 @@ export default function Home() {
     <Card>
       <CardContent>
         <FormikStepper
-          validationSchema={object({
-            money: mixed().when("millionaire", {
-              is: true,
-              then: number()
-                .required()
-                .min(1_000_000, "You must have 1,000,000 to be a millionaire"),
-              otherwise: number().required(),
-            }),
-          })}
+          
           initialValues={{
             firstname: "",
             lastName: "",
@@ -28,7 +21,7 @@ export default function Home() {
           }}
           onSubmit={() => {}}
         >
-          <div>
+          <FormikStep>
             <Field name="firstName" component={TextField} label="First Name" />
             <Field name="lastName" component={TextField} label="Last Name" />
             <Field
@@ -37,38 +30,71 @@ export default function Home() {
               component={CheckboxWithLabel}
               Label={{ label: "I am a Millionaire" }}
             />
-          </div>
+          </FormikStep>
 
-          <div>
+          <FormikStep validationSchema={object({
+            money: mixed().when("millionaire", {
+              is: true,
+              then: number()
+                .required()
+                .min(1_000_000, "You must have 1,000,000 to be a millionaire"),
+              otherwise: number().required(),
+            }),
+          })}>
             <Field
               name="money"
               type="number"
               component={TextField}
               label="All the money I have"
             />
-          </div>
+          </FormikStep>
 
-          <div>
+          <FormikStep>
             <Field
               name="description"
               component={TextField}
               label="Description"
             />
-          </div>
+          </FormikStep>
         </FormikStepper>
       </CardContent>
     </Card>
   );
 }
 
+export interface FormikStepProps extends Pick<FormikConfig<FormikValues>, 'children' | 'validationSchema'> {
+    
+}
+
+export function FormikStep({ children }: FormikStepProps){
+    return <>{children}</>
+}
+
 export function FormikStepper({children, ...props}: FormikConfig<FormikValues>) {
-    const childrenArray = React.Children.toArray(children);
+    const childrenArray = React.Children.toArray(children) as React.ReactElement<FormikStepProps>[];
     const [step, setStep] = useState(0);
     const currentChild = childrenArray[step];
 
+
+    function isLastStep() {
+        return step === childrenArray.length - 1;
+    }
     return (
-        <Formik {...props}>
-            <Form autoComplete="off">{children}</Form>
+        <Formik {...props} 
+        validationSchema={currentChild.props.validationSchema}
+        onSubmit={ async(values, helpers) => {
+            if(isLastStep()) {
+                await props.onSubmit(values, helpers);
+            } else {
+                setStep(step => step +1);
+            }
+        }}>
+            <Form autoComplete="off">{currentChild}</Form>
+            {step > 0 ? 
+                <Button onClick={() => setStep(step => step - 1)}>Back</Button> 
+                : null
+            }
+            <Button type="submit">{isLastStep() ? 'Submit' : 'Next'}</Button>
         </Formik>
     );
 }
